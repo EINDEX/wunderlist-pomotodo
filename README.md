fe391a3d8f3c2301b5cc4a7e256ea722b0b98725
EINDEX <snowstarlbk@gmail.com>
2017-04-23 21:28:33 +0800

    - 支持日期/时间 - 支持 -t[number] 特性，来添加预计番茄数量 - 将 wunderlist的 id 移动到内容标签结尾，使观感更干净 - 支持 wunderlist 内容的同步

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
-# wunderlist-pomotodo
+# Auto Pomotodo
+更好用的 todo 应用通过 Webhooks 同步至番茄土豆,使 Pomotodo 使用起来更简单，方便。
+### 支持的清单应用
+- Wunderlist
 
-将奇妙清单和番茄土豆链接到一起
+### 使用要求
 
-## 使用要求
+- Pomotodo vip 用户
+- Wunderlist 用户
 
-- pomotodo vip 用户
-- wunderlist 用户
+### 功能简介
+
+- Wunderlist
+    - Task 同步
+        - 内容
+        - Star 标志
+        - 提醒
+        - 笔记
+    - Web 端选择需要更新的同步清单
+
+### Todo
+- Wunderlist
+    - 子任务同步
+    - 网页上修改同步清单
+
+### LISENSE
+
+GPL v3
--- auto-pomotodo/auto_pomotodo.py
+++ auto-pomotodo/auto_pomotodo.py
@@ -1,55 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-PyCharm : 
-Copyright (C) 2016-2017 EINDEX Li
-
-@Author        : EINDEX Li
-@File          : auto_pomotodo.py
-@Created       : 2017/3/31
-@Last Modified : 2017/3/31
-"""
-from flask import Flask, request, json
-from requests import Session
-import os
-
-app = Flask(__name__)
-
-pomotodo_key = os.getenv('POMOTODO_KEY')
-
-session = Session()
-session.headers.update({'Authorization': f'token {pomotodo_key}'})
-
-
-@app.route('/')
-def index():
-    return 'auto pomotodo'
-
-
-@app.route('/webhooks', methods=['POST'])
-def webhooks():
-    operation = request.json['operation']
-    todo_id = request.json['after']['id']
-    title = request.json['after']['title']
-    params = {
-        'description': f'{title} ${todo_id}$',
-        'pin': request.json['after']['starred'],
-        'completed': request.json['after']['completed'],
-    }
-    if operation == 'create':
-        session.post('https://api.pomotodo.com/1/todos', params)
-    elif operation == 'update':
-        todos = json.loads(session.get('https://api.pomotodo.com/1/todos').content.decode())
-        for todo in [todo for todo in todos if f'{todo_id}' in todo['description']]:
-            r = session.patch(f'https://api.pomotodo.com/1/todos/{todo["uuid"]}', params)
-    elif operation == 'delete':
-        from pprint import pprint
-        todos = json.loads(session.get('https://api.pomotodo.com/1/todos').content.decode())
-        for todo in [todo for todo in todos if f'${todo_id}$' in todo['description']]:
-            r = session.delete(f'https://api.pomotodo.com/1/todos/{todo["uuid"]}')
-            pprint(r.text)
-    return 'ok'
-
-
-if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+#!/usr/bin/env python# -*- coding: utf-8 -*-"""PyCharm : Copyright (C) 2016-2017 EINDEX Li@Author        : EINDEX Li@File          : auto_pomotodo.py@Created       : 2017/3/31@Last Modified : 2017/3/31"""from flask import Flask, request, render_templatefrom requests import Sessionimport osimport jsonimport reimport dateutil.parserapp = Flask(__name__)app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')pomotodo_key = os.getenv('POMOTODO_KEY')wunderlist_access_token = os.getenv('WUNDERLIST_ACCESS_TOKEN')wunderlist_client_id = os.getenv('WUNDERLIST_CLIENT_ID')session = Session()session.headers.update({'Authorization': f'token {pomotodo_key}',                        'X-Access-Token': wunderlist_access_token,                        'X-Client-ID': wunderlist_client_id})@app.route('/')def index():    return render_template('index.html')@app.route('/user')def user():    r = session.get('http://a.wunderlist.com/api/v1/lists')    data = json.loads(r.content.decode())    for i, item in enumerate(data):        r = session.get('http://a.wunderlist.com/api/v1/webhooks', params={'list_id': item['id']})        if json.loads(r.content.decode()):            data[i]['webhooks'] = True    return render_template('user.html', data=data)@app.route('/user/list/<id>', methods=['PUT'])def user_list(id):    if request.json['checked']:        r = session.post('http://a.wunderlist.com/api/v1/webhooks',                         params={'list_id': id,                                 'url': 'http://pomotodo.eindex.me/webhooks',                                 'processor_type': 'generic',                                 'configuration': ''})    else:        r = session.delete(f'http://a.wunderlist.com/api/v1/webhooks/{id}', params={'revision': 0})    return r.content.decode()def get_wunderlist_info(todo_id):    data = session.get(f'http://a.wunderlist.com/api/v1/tasks/{todo_id}').json()    r = session.get('http://a.wunderlist.com/api/v1/notes', params={'task_id': todo_id})    if r.ok:        data['content'] = json.loads(r.content.decode())[0]['content']    r = session.get('http://a.wunderlist.com/api/v1/reminders', params={'task_id': todo_id})    if r.ok:        data['date'] = json.loads(r.content.decode())[0]['date']    return datadef wunderlist_webhooks_json(func):    def wrapper(todo_id):        params = {}        data = get_wunderlist_info(todo_id)        if 'title' in data:            params['description'] = data['title']            re_pomo_count = re.search(r'-t[0-9]{1,}', params['description'])            if re_pomo_count:                params['description'] = ''.join([params['description'][:re_pomo_count.start()],                                                 params['description'][re_pomo_count.end():]])                params['estimated_pomo_count'] = re_pomo_count.group(0)[2:]        if 'content' in data:            params['notice'] = f"{data['content']} ${todo_id}"        else:            params['notice'] = f"${todo_id}"        if 'starred' in data:            params['pin'] = data['starred']        if 'completed' in data:            params['completed'] = data['completed']        if 'date' in data:            params['remind_time'] = dateutil.parser.parse(data['date'])        return func(todo_id=todo_id, params=params)    return wrapperdef pomo_get(todo_id):    todos = json.loads(session.get('https://api.pomotodo.com/1/todos').content.decode())    for todo in todos:        if not todo['notice']:            continue        m = r'%s' % todo_id        if re.search(m, todo['notice']):            return todo@wunderlist_webhooks_jsondef pomo_create(todo_id, params):    session.post('https://api.pomotodo.com/1/todos', params)@wunderlist_webhooks_jsondef pomo_update(todo_id, params):    todo = pomo_get(todo_id)    if todo:        session.patch(f'https://api.pomotodo.com/1/todos/{todo["uuid"]}', params)def pomo_delete(todo_id):    todo = pomo_get(todo_id)    if todo:        session.delete(f'https://api.pomotodo.com/1/todos/{todo["uuid"]}')@app.route('/webhooks', methods=['POST'])def wunderlist_webhooks():    operation = request.json['operation']    todo_id = request.json['after']['id']    if operation == 'create':        pomo_create(todo_id)    elif operation == 'update':        pomo_update(todo_id)    elif operation == 'delete':        pomo_delete(todo_id)    return 'ok'if __name__ == '__main__':    app.run(host='0.0.0.0', port=5001, debug=True)
